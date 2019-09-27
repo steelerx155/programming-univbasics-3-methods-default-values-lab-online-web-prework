7aca34c0304281df018abcd3977ee45338acc17b
Randy Steele <steelerx155@gmail.com>
2019-09-27 19:47:01 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 def meal_choice(veg1, veg2, protein = "chicken")
   puts "What a nutricious meal!"
-  puts "A plate of #{protein}" with #{veg1} and #{veg2}.} so that you can also see what you've ordered."
+  puts "A plate of #{protein}" with #{veg1} and #{veg2}."so that you can also see what you've ordered.
 end
 
 meal_choice("Carrots", "Corn")
